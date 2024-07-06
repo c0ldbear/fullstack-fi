bb804ce249a74769219e98d61507af59e97a126c
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-06 12:14:53 +0200

    Refactoring the components and add logs to understand code

--- part1/follow-along/src/App.jsx
+++ part1/follow-along/src/App.jsx
@@ -1,21 +1,43 @@
 // Time for Component state, event handlers
-// https://fullstackopen.com/en/part1/component_state_event_handlers 
-import { useState } from 'react'
+// https://fullstackopen.com/en/part1/component_state_event_handlers
+import { useState } from "react";
 
+const Display = ({counter}) => <div>{counter}</div>;
+
+const Button = ({onClick, text}) => {
+  return <button onClick={onClick}>{text}</button>;
+};
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  const increaseByOne = () => setCounter(counter + 1)
-  const setToZero = () => setCounter(0)
+  const [counter, setCounter] = useState(0);
+  console.log(">> rendering with counter value: ", counter);
 
-  console.log("rendering...", counter)
+  const increaseByOne = () => {
+    console.log(">>> increasing, value before: ", counter)
+    setCounter(counter + 1)
+  };
+
+  const decreaseByOne = () => {
+    console.log(">>> decreasing, value before: ", counter)
+    setCounter(counter - 1)
+  };
+
+  const setToZero = () => {
+    console.log(">>> resetting to zero, value before: ", counter)
+    setCounter(0)
+  };
+
+  console.log(">> rendering...", counter);
 
   return (
     <div>
-    <button onClick={increaseByOne}>plus</button>
-    <button onClick={setToZero}>zero</button>
+      <Display counter={counter} />
+      <br />
+      <Button onClick={increaseByOne} text={"plus"} />
+      <Button onClick={setToZero} text={"zero"} />
+      <Button onClick={decreaseByOne} text={"minus"} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
