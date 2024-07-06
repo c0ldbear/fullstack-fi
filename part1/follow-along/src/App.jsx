8651258780404800b17412fa3c7d5f790c9257af
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-06 12:16:34 +0200

    Refactor Button component

--- part1/follow-along/src/App.jsx
+++ part1/follow-along/src/App.jsx
@@ -2,29 +2,31 @@
 // https://fullstackopen.com/en/part1/component_state_event_handlers
 import { useState } from "react";
 
-const Display = ({counter}) => <div>{counter}</div>;
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = ({onClick, text}) => {
-  return <button onClick={onClick}>{text}</button>;
-};
+const Button = ({ onSmash, text }) => (
+  <button onClick={onSmash}>
+    {text}
+  </button>
+);
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   console.log(">> rendering with counter value: ", counter);
 
   const increaseByOne = () => {
-    console.log(">>> increasing, value before: ", counter)
-    setCounter(counter + 1)
+    console.log(">>> increasing, value before: ", counter);
+    setCounter(counter + 1);
   };
 
   const decreaseByOne = () => {
-    console.log(">>> decreasing, value before: ", counter)
-    setCounter(counter - 1)
+    console.log(">>> decreasing, value before: ", counter);
+    setCounter(counter - 1);
   };
 
   const setToZero = () => {
-    console.log(">>> resetting to zero, value before: ", counter)
-    setCounter(0)
+    console.log(">>> resetting to zero, value before: ", counter);
+    setCounter(0);
   };
 
   console.log(">> rendering...", counter);
