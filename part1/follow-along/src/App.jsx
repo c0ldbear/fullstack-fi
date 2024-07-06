d39d86d5cad40955323b462eacb1dd46f5ac44d4
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-06 12:02:47 +0200

    Move the event handlers to their own variables

--- part1/follow-along/src/App.jsx
+++ part1/follow-along/src/App.jsx
@@ -5,13 +5,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
 
   console.log("rendering...", counter)
 
   return (
     <div>
-    <button onClick={() => setCounter(counter + 1)}>plus</button>
-    <button onClick={() => setCounter(0)}>zero</button>
+    <button onClick={increaseByOne}>plus</button>
+    <button onClick={setToZero}>zero</button>
     </div>
   )
 }
