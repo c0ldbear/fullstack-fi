9bbf5205f64715be56298c4a779adfb35c42ee3b
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-06 11:56:36 +0200

    Rerendering with timeout in react component

--- part1/follow-along/src/App.jsx
+++ part1/follow-along/src/App.jsx
@@ -1,25 +1,18 @@
 // Time for Component state, event handlers
 // https://fullstackopen.com/en/part1/component_state_event_handlers 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-    </div>
-  )
-}
+import { useState } from 'react'
 
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
-  return (
-    <div>
-      <p>Hello {name}! You are {age} years old.</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
 
-const App = (props) => {
-  const {counter} = props
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log("rendering...", counter)
+
   return (
     <div>
       {counter}
