46aa70bc26703e2ada1b4f062473ee667d6d5121
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-06 12:22:24 +0200

    Add a more complex state to handle

--- part1/follow-along/src/App.jsx
+++ part1/follow-along/src/App.jsx
@@ -4,19 +4,37 @@
 import { useState } from "react";
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  // handling of a more complex state
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
+      {clicks.left}
+      <button onClick={handleLeftClick}>
         left
       </button>
-      <button onClick={() => setRight(right + 1)}>
+      <button onClick={handleRightClick}>
         right
       </button>
-      {right}
+      {clicks.right}
     </div>
   )
 };
