857f0edb6e12d2fd2888ab13c054f6820f6a9507
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-06 12:17:58 +0200

    Add start code for part 1d

--- part1/follow-along/src/App.jsx
+++ part1/follow-along/src/App.jsx
@@ -1,41 +1,24 @@
-// Time for Component state, event handlers
-// https://fullstackopen.com/en/part1/component_state_event_handlers
+// Time for "A more complex state, debugging React apps" 
+// https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps
+
 import { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>;
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log(">> rendering with counter value: ", counter);
-
-  const increaseByOne = () => {
-    console.log(">>> increasing, value before: ", counter);
-    setCounter(counter + 1);
-  };
-
-  const decreaseByOne = () => {
-    console.log(">>> decreasing, value before: ", counter);
-    setCounter(counter - 1);
-  };
-
-  const setToZero = () => {
-    console.log(">>> resetting to zero, value before: ", counter);
-    setCounter(0);
-  };
-
-  console.log(">> rendering...", counter);
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   return (
     <div>
-      <Display counter={counter} />
-      <br />
-      <Button onClick={increaseByOne} text={"plus"} />
-      <Button onClick={setToZero} text={"zero"} />
-      <Button onClick={decreaseByOne} text={"minus"} />
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
-  );
+  )
 };
 
 export default App;
