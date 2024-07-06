dab22e6d4dc7ff7f1c0349f911de00d8462003e4
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-06 12:31:23 +0200

    Update complex state and add a forbidden way of doing it

--- part1/follow-along/src/App.jsx
+++ part1/follow-along/src/App.jsx
@@ -1,4 +1,4 @@
-// Time for "A more complex state, debugging React apps" 
+// Time for "A more complex state, debugging React apps"
 // https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps
 
 import { useState } from "react";
@@ -6,37 +6,39 @@ import { useState } from "react";
 const App = () => {
   // handling of a more complex state
   const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+    left: 0,
+    right: 0,
+  });
 
-  const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1,
-    }
-    setClicks(newClicks)
-  }
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
 
   const handleRightClick = () => {
     const newClicks = {
       ...clicks,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
+      right: clicks.right + 1,
+    };
+    setClicks(newClicks); 
+  };
+
+  // in react, it is forbidden to mutate the state directly!
+  // Forbidden:
+  const handleLeftClickForbidden = () => {
+    clicks.left++
+    setClicks(clicks)
   }
+
+  const setClicksToZero = () => setClicks({left: 0, right: 0})
 
   return (
     <div>
       {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
+      <button onClick={handleLeftClickForbidden}>left</button>
+      <button onClick={handleRightClick}>right</button>
       {clicks.right}
+      <br />
+      <button onClick={setClicksToZero}>zero</button>
     </div>
-  )
+  );
 };
 
 export default App;
