2bc36fcd63bb84638e3e28e5cf015fa5def50c53
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-08 20:22:56 +0200

    Add start for 'unicafe' exercises

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return <div>Code here</div>;
+}
+
+export default App;
