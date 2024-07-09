664342d2070c423b60238a4f58ad1d35b997d6c4
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-09 08:14:07 +0200

    Add solution to 1.13 (votes to anecdotes)
    Refactor 'anecdotes' to be a state, and change it to be an object so
    that the points are stored "closer" to the anecdote.

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -5,27 +5,48 @@ const Button = ({ text, onClick }) => {
 };
 
 function App() {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
-
+  const [anecdotes, setAnecdotes] = useState({
+    data: [
+      { text: "If it hurts, do it more often.", points: 0 },
+      { text: "Adding manpower to a late software project makes it later!", points: 0 },
+      {
+        text: "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+        points: 0,
+      },
+      {
+        text: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        points: 0,
+      },
+      { text: "Premature optimization is the root of all evil.", points: 0 },
+      {
+        text: "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+        points: 0,
+      },
+      {
+        text: "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+        points: 0,
+      },
+      { text: "The only way to go fast, is to go well.", points: 0 },
+    ],
+  });
   const [selected, setSelected] = useState(0);
 
   const generateRandomAnecdote = () => {
-    const randomAnecdote = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomAnecdote);
+    const randomAnecdoteIndex = Math.floor(Math.random() * anecdotes.data.length);
+    setSelected(randomAnecdoteIndex);
+  };
+
+  const setPointsToAnecdote = () => {
+    const copy = { ...anecdotes };
+    copy.data[selected].points += 1;
+    setAnecdotes(copy);
   };
 
   return (
     <>
-      <div>{anecdotes[selected]}</div>
+      <div>{anecdotes.data[selected].text}</div>
+      <div>has {anecdotes.data[selected].points} votes</div>
+      <Button text={"vote"} onClick={setPointsToAnecdote} />
       <Button text={"next anecdote"} onClick={generateRandomAnecdote} />
     </>
   );
