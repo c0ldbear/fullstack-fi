abbece133634f116e726e49d541e4b1fd03adf72
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-10 15:57:26 +0200

    Add solution for 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,58 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => {
+        return <Part key={part.id} name={part.name} exercises={part.exercises} />;
+      })}
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+function App() {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+}
+
+export default App;
