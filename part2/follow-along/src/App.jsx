d3f4445b0136f0aee9eaaff53bfd37edf00ce428
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-10 16:40:13 +0200

    Add different onXX handling stuff because it was fun! :)
    when we added the 'value' attribute to the input and set the value
    to be our state, so we needed to add the 'onChange' too to be
    able to control it. Define what should happen when we have a value
    on the input. Otherwise React(?) will block the functionality
    for us, since the 'value' attribute is set to our State ('newNote')

--- part2/follow-along/src/App.jsx
+++ part2/follow-along/src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Note from "./components/Note";
 
-function App({ notes }) {
+function App(props) {
   // We will use functional programming operators now of the JS _array_,
   // such as find, filter, and map
+  const defaultInputText = "a new note ...";
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState(defaultInputText);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(">> event.target.value", event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const handleOnNoteFocus = (event) => {
+    console.log(">> event", event);
+    setNewNote("");
+  };
+
+  const handleOnNoteBlur = (event) => {
+    setNewNote(defaultInputText);
+  };
 
   return (
     <div>
@@ -13,6 +37,15 @@ function App({ notes }) {
           <Note key={note.id} note={note} /> // NOTE (lol) that the 'key' attribute now must be defined for the Note components, and not for the li tags like before.
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+          onFocus={handleOnNoteFocus}
+          onBlur={handleOnNoteBlur}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 }
