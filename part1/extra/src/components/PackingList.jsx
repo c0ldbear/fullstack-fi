a78e5cbab3d645a611ae9f3ddfb2b1bda2f3f500
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-10 10:00:15 +0200

    Use the logical AND operator (&&) to render the checkmark

--- part1/extra/src/components/PackingList.jsx
+++ part1/extra/src/components/PackingList.jsx
@@ -1,5 +1,7 @@
 function Item({ name, isPacked }) {
-  return <li className="item">{isPacked ? (<del>{name + " ✔" }</del>): (name)} </li>;
+    return (<li className="item">
+        {name} {isPacked && '✔'}
+    </li>)
 }
 
 export default function PackingList() {
