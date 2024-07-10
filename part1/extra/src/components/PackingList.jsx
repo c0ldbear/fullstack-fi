d356bf76122e1d37ffd3e83cc66175d5858d042d
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-10 09:58:05 +0200

    Add a html tag to the component within the ternary operator

--- part1/extra/src/components/PackingList.jsx
+++ part1/extra/src/components/PackingList.jsx
@@ -1,5 +1,5 @@
 function Item({ name, isPacked }) {
-  return <li className="item">{isPacked ? name + " ✔" : name} </li>;
+  return <li className="item">{isPacked ? (<del>{name + " ✔" }</del>): (name)} </li>;
 }
 
 export default function PackingList() {
