94116449029486d5b37dbdd8e8cb5b05425c8bb7
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-10 16:57:56 +0200

    Add start for exercises in 2b (2.6 - 2.10)

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => {
+        return <p key={index}>{person.name}</p>;
+      })}
+    </div>
+  );
+};
+
+export default App;
