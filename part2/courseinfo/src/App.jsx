501b9a34e52f7ad77f097ab6a01e354b157bd50e
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-10 16:15:17 +0200

    Add solution to 2.5 (move Course to own file)

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,49 +1,7 @@
+import Course from "./components/Course";
+
 const Header = ({ text }) => {
   return <h1>{text}</h1>;
-};
-
-const CourseTitle = ({ name }) => {
-  return <h2>{name}</h2>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => {
-        return <Part key={part.id} name={part.name} exercises={part.exercises} />;
-      })}
-      <SumOfExercises parts={parts} />
-    </div>
-  );
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const SumOfExercises = ({ parts }) => {
-  const exercises = parts.map((part) => {
-    return part.exercises;
-  });
-  let sum = exercises.reduce((sum, value) => sum + value, 0);
-  return (
-    <p>
-      <b>total of {sum} exercises</b>
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <CourseTitle name={course.name} />
-      <Content parts={course.parts} />
-    </>
-  );
 };
 
 function App() {
