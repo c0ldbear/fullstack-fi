0da46ec569426ba95c6fff350a31b5d08ddf52e3
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-10 09:45:42 +0200

    Add a first conditional rendering for a component

--- part1/extra/src/components/PackingList.jsx
+++ part1/extra/src/components/PackingList.jsx
@@ -1,16 +1,20 @@
 function Item({ name, isPacked }) {
-    return <li className="item">{name}</li>
+  if (isPacked) {
+    return <li className="item">{name} ✔</li>;
+  }
+
+  return <li className="item">{name}</li>;
 }
 
 export default function PackingList() {
-    return (
-        <section>
-            <h1>Sally Ride's Packing List</h1>
-            <ul>
-                <Item isPacked={true} name="space suit" />
-                <Item isPacked={true} name="helmet with a golden leaf" />
-                <Item isPacked={false} name="photo of tam" />
-            </ul>
-        </section>
-    )
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={false} name="space suit" />
+        <Item isPacked={true} name="helmet with a golden leaf" />
+        <Item isPacked={false} name="photo of tam" />
+      </ul>
+    </section>
+  );
 }
