f3655629add032f399aa736d0b49a2a826a4a555
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-17 10:28:00 +0200

    Add the rendering of App to the success of axios
    NOT a good way of doing it.

--- part2/follow-along/src/main.jsx
+++ part2/follow-along/src/main.jsx
@@ -1,39 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import App from "./App.jsx";
 import "./index.css";
 
-import axios from "axios";
-
+//    This approach has many issues, as we are rendering the entire App component
+//  only when we successfully retrieve a response.
 axios.get("http://localhost:3001/notes").then((res) => {
   console.log(">> response:", res);
   const notes = res.data;
   console.log(">>> data (notes):", notes);
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App notes={notes} />
+    </React.StrictMode>
+  );
 });
-
-const promise2 = axios.get("http://localhost:3001/foobar");
-console.log(">> promise2:", promise2);
-
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App notes={notes} />
-  </React.StrictMode>
-);
