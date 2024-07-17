108d6d3744ae58e2f43d7fb244829cf7b90e66bb
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2024-07-17 10:20:36 +0200

    Add axios to call the endpoint for our notes

--- part2/follow-along/src/main.jsx
+++ part2/follow-along/src/main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import axios from "axios";
+
+const promise = axios.get("http://localhost:3001/notes");
+console.log(">> promise:", promise);
+promise.then((res) => {
+  console.log(">> response:", res);
+  console.log(">>> data:", res.data);
+});
+
+const promise2 = axios.get("http://localhost:3001/foobar");
+console.log(">> promise2:", promise2);
+
 const notes = [
   {
     id: 1,
